5ab925328448d6ba0d2531d7f975a50fda954e82
sahil <mdsahil0170@gmail.com>
2021-09-02 21:28:58 +0600

    Version 1

---
+++ login.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+
+win = tk.Tk()
+win.geometry("400x500+450+50")
+win.title("Login Form")
+win.wm_iconbitmap(r"G:\icon\vscode.ico")
+
+
+
+
+win.mainloop()
