69ac912b9b502d6329263209fef40fd44060ba50
sahil <mdsahil0170@gmail.com>
2021-09-03 10:52:03 +0600

    Add Command

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+# this is import part
 import tkinter as tk
 from tkinter import StringVar, ttk
 
@@ -6,27 +7,29 @@ win.geometry("400x500+450+50")
 win.title("Login Form")
 win.wm_iconbitmap(r"G:\icon\vscode.ico")
 
+
 def submit(event=None):
     var1 = name_entry.get()
-    var2 =passowrd_entry.get()
-    if var1=='' or var2=='':
-        print("pleas enter alphbat",var2,var1)
+    var2 = passowrd_entry.get()
+    if var1 == '' or var2 == '':
+        print("pleas enter alphbat", var2, var1)
     else:
-        with open('text.txt','a') as f:
+        with open('text.txt', 'a') as f:
             f.write(f'Name : {var1} And Passowrd : {var2} \n')
         f.close()
         name_entry.set('')
         passowrd_entry.set('')
- 
-login_form = ttk.Label(win,text='Login Form',font='arial 20')
-user_name = ttk.Label(win,text='User Name',font='arial 10')
-name_entry=StringVar()
-user_name_entry = ttk.Entry(win,textvariable=name_entry)
+
+
+login_form = ttk.Label(win, text='Login Form', font='arial 20')
+user_name = ttk.Label(win, text='User Name', font='arial 10')
+name_entry = StringVar()
+user_name_entry = ttk.Entry(win, textvariable=name_entry)
 user_name_entry.focus()
-user_passowrd = ttk.Label(win,text='User Passowrd',font='arial 10')
-passowrd_entry=StringVar()
-user_passowrd_entry = ttk.Entry(win,textvariable=passowrd_entry)
-submit = ttk.Button(win,text='SUBMIT',command=submit)
+user_passowrd = ttk.Label(win, text='User Passowrd', font='arial 10')
+passowrd_entry = StringVar()
+user_passowrd_entry = ttk.Entry(win, textvariable=passowrd_entry)
+submit = ttk.Button(win, text='SUBMIT', command=submit)
 
 login_form.pack()
 user_name.pack()
